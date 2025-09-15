087b1feb33765ee22a97b59659dd2f432d501409
AntonCh2025 <darth-filth@yandex.ru>
2025-09-15 12:46:31 +0300

    Исправление по ревью: Созданы тестовые классы в каждом модуле. Тесты размещены в классах

--- tests/test_create_ad.py
+++ tests/test_create_ad.py
@@ -6,54 +6,57 @@ from data.locators import StartPageLocators as locator
 from data.data import TestDataStatic as td
 from data.helpers import TestDataCreation as new
 
-#Создание объявления неавторизованным пользователем
-def test_create_ad_unauthorized_user_error(browser):
 
-    # Нажать кнопку «Разместить объявление».
-    browser.find_element(*locator.post_an_ad_button).click()
+class TestCreateAd:
 
-    # Проверить: отображается модальное окно с заголовком «Чтобы разместить объявление, авторизуйтесь».
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.need_authorization_modal_header))
-    modal_window_is_displayed = browser.find_element(*locator.need_authorization_modal).is_displayed()
-    header_text_is_correct = (browser.find_element(*locator.need_authorization_modal_header).text == 'Чтобы разместить объявление, авторизуйтесь')
+    #Создание объявления неавторизованным пользователем
+    def test_create_ad_unauthorized_user_error(self, browser):
 
-    assert modal_window_is_displayed and header_text_is_correct
+        # Нажать кнопку «Разместить объявление».
+        browser.find_element(*locator.post_an_ad_button).click()
 
-#Создание объявления авторизованным пользователем
-def test_create_ad_authorized_user_success(new_user, browser):
-    # Фикстура new_user зарегистрировала нового пользователя. В тест приходит уже залогиненная под этим пользователем страница
-    # Нажать кнопку "Разместить объявление"
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.avatar))
-    browser.find_element(*locator.post_an_ad_button).click()
+        # Проверить: отображается модальное окно с заголовком «Чтобы разместить объявление, авторизуйтесь».
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.need_authorization_modal_header))
+        modal_window_is_displayed = browser.find_element(*locator.need_authorization_modal).is_displayed()
+        header_text_is_correct = (browser.find_element(*locator.need_authorization_modal_header).text == 'Чтобы разместить объявление, авторизуйтесь')
 
-    # Заполнить все поля формы: «Название», «Описание товара», «Стоимость» — стоимость должна быть указана в числовом формате.
-    new_good = new.new_good()
+        assert modal_window_is_displayed and header_text_is_correct
 
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.new_good_description))
-    browser.find_element(*locator.new_good_name).send_keys(new_good['name'])
-    browser.find_element(*locator.new_good_description).send_keys(new_good['description'])
-    browser.find_element(*locator.new_good_price).send_keys(new_good['price'])
+    #Создание объявления авторизованным пользователем
+    def test_create_ad_authorized_user_success(self, new_user, browser):
+        # Фикстура new_user зарегистрировала нового пользователя. В тест приходит уже залогиненная под этим пользователем страница
+        # Нажать кнопку "Разместить объявление"
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.avatar))
+        browser.find_element(*locator.post_an_ad_button).click()
 
-    # Выбрать из Dropdown «Категорию» и «Город».
-    browser.find_element(*locator.new_good_category_button).click()
-    browser.find_element(By.XPATH, new_good['category_locator']).click()
-    browser.find_element(*locator.new_good_city_button).click()
-    browser.find_element(By.XPATH, new_good['city_locator']).click()
+        # Заполнить все поля формы: «Название», «Описание товара», «Стоимость» — стоимость должна быть указана в числовом формате.
+        new_good = new.new_good()
 
-    # Выбрать RabioButton «Состояние товара».
-    browser.find_element(By.XPATH, new_good['condition_locator']).click()
-    
-    # Нажать кнопку «Опубликовать».
-    browser.find_element(*locator.new_good_publish_button).click()
-    
-    # Перейти в профиль пользователя.
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.next_page_button))
-    browser.find_element(*locator.avatar).click()
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.new_good_description))
+        browser.find_element(*locator.new_good_name).send_keys(new_good['name'])
+        browser.find_element(*locator.new_good_description).send_keys(new_good['description'])
+        browser.find_element(*locator.new_good_price).send_keys(new_good['price'])
 
-    # Проверить: в блоке «Мои объявления» отображается созданное объявление.
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.my_ad_name))
-    new_ad_name = browser.find_element(*locator.my_ad_name).text
-    new_ad_city = browser.find_element(*locator.my_ad_city).text
+        # Выбрать из Dropdown «Категорию» и «Город».
+        browser.find_element(*locator.new_good_category_button).click()
+        browser.find_element(By.XPATH, new_good['category_locator']).click()
+        browser.find_element(*locator.new_good_city_button).click()
+        browser.find_element(By.XPATH, new_good['city_locator']).click()
 
-    assert new_ad_name == new_good['name'] and new_ad_city == new_good['city']
-    
+        # Выбрать RabioButton «Состояние товара».
+        browser.find_element(By.XPATH, new_good['condition_locator']).click()
+        
+        # Нажать кнопку «Опубликовать».
+        browser.find_element(*locator.new_good_publish_button).click()
+        
+        # Перейти в профиль пользователя.
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.next_page_button))
+        browser.find_element(*locator.avatar).click()
+
+        # Проверить: в блоке «Мои объявления» отображается созданное объявление.
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.my_ad_name))
+        new_ad_name = browser.find_element(*locator.my_ad_name).text
+        new_ad_city = browser.find_element(*locator.my_ad_city).text
+
+        assert new_ad_name == new_good['name'] and new_ad_city == new_good['city']
+        
--- tests/test_login_logout.py
+++ tests/test_login_logout.py
@@ -4,46 +4,48 @@ from data.locators import StartPageLocators as locator
 from data.data import TestDataStatic as td
 
 
-#Login пользователя
-def test_login_user_exist_success(browser):
+class TestLoginLogout:
 
-    # Нажать кнопку «Вход и регистрация».
-    browser.find_element(*locator.enter_and_register_button).click()
+    #Login пользователя
+    def test_login_user_exist_success(self, browser):
 
-    # Заполнить все поля формы авторизации и нажать кнопку «Войти».
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.ent_enter_button))
-    browser.find_element(*locator.ent_email_input).send_keys(td.user_exist['login'])
-    browser.find_element(*locator.ent_password_input).send_keys(td.user_exist['password'])
-    browser.find_element(*locator.ent_enter_button).click()
+        # Нажать кнопку «Вход и регистрация».
+        browser.find_element(*locator.enter_and_register_button).click()
 
-    # Проверить: произошёл переход на главную страницу, 
-    #   в правом верхнем углу около кнопки «Разместить объявление» отображается аватар пользователя и имя User.
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.avatar))
-    avatar_is_displayed = browser.find_element(*locator.avatar).is_displayed()
-    user_name_is_displayed = browser.find_element(*locator.user_name).is_displayed()
-    assert avatar_is_displayed and user_name_is_displayed
+        # Заполнить все поля формы авторизации и нажать кнопку «Войти».
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.ent_enter_button))
+        browser.find_element(*locator.ent_email_input).send_keys(td.user_exist['login'])
+        browser.find_element(*locator.ent_password_input).send_keys(td.user_exist['password'])
+        browser.find_element(*locator.ent_enter_button).click()
 
-#Logout пользователя
-def test_logout_user_exist_success(browser):
-    # Авторизоваться под заранее созданным пользователем.
-    # Нажать кнопку «Вход и регистрация».
-    browser.find_element(*locator.enter_and_register_button).click()
+        # Проверить: произошёл переход на главную страницу, 
+        #   в правом верхнем углу около кнопки «Разместить объявление» отображается аватар пользователя и имя User.
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.avatar))
+        avatar_is_displayed = browser.find_element(*locator.avatar).is_displayed()
+        user_name_is_displayed = browser.find_element(*locator.user_name).is_displayed()
+        assert avatar_is_displayed and user_name_is_displayed
 
-    # Заполнить все поля формы авторизации и нажать кнопку «Войти».
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.ent_enter_button))
-    browser.find_element(*locator.ent_email_input).send_keys(td.user_exist['login'])
-    browser.find_element(*locator.ent_password_input).send_keys(td.user_exist['password'])
-    browser.find_element(*locator.ent_enter_button).click()
+    #Logout пользователя
+    def test_logout_user_exist_success(self, browser):
+        # Авторизоваться под заранее созданным пользователем.
+        # Нажать кнопку «Вход и регистрация».
+        browser.find_element(*locator.enter_and_register_button).click()
 
-    # Нажать кнопку «Выйти».
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.exit_profile_button))
-    browser.find_element(*locator.exit_profile_button).click()
+        # Заполнить все поля формы авторизации и нажать кнопку «Войти».
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.ent_enter_button))
+        browser.find_element(*locator.ent_email_input).send_keys(td.user_exist['login'])
+        browser.find_element(*locator.ent_password_input).send_keys(td.user_exist['password'])
+        browser.find_element(*locator.ent_enter_button).click()
 
-    # Проверить: аватар пользователя и имя User больше не отображается в правом верхнем углу около кнопки «Разместить объявление», 
-    #     там теперь отображается кнопка «Вход и регистрация».
-    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.enter_and_register_button))
-    avatar_not_displayed = (len(browser.find_elements(*locator.avatar)) == 0)
-    user_name_not_displayed = (len(browser.find_elements(*locator.user_name)) == 0)
-    enter_and_registration_button_is_displayed = browser.find_element(*locator.enter_and_register_button).is_displayed()
+        # Нажать кнопку «Выйти».
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.exit_profile_button))
+        browser.find_element(*locator.exit_profile_button).click()
 
-    assert avatar_not_displayed and user_name_not_displayed and enter_and_registration_button_is_displayed
+        # Проверить: аватар пользователя и имя User больше не отображается в правом верхнем углу около кнопки «Разместить объявление», 
+        #     там теперь отображается кнопка «Вход и регистрация».
+        WebDriverWait(browser, 3).until(EC.visibility_of_element_located(locator.enter_and_register_button))
+        avatar_not_displayed = (len(browser.find_elements(*locator.avatar)) == 0)
+        user_name_not_displayed = (len(browser.find_elements(*locator.user_name)) == 0)
+        enter_and_registration_button_is_displayed = browser.find_element(*locator.enter_and_register_button).is_displayed()
+
+        assert avatar_not_displayed and user_name_not_displayed and enter_and_registration_button_is_displayed
